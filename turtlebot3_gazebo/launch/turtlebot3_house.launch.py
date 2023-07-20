ccc848735d6c3a3343727dfe171e3970558f7b75
redsIng <lorenzo.ing.rossi@gmail.com>
2023-07-20 19:21:52 +0200

    added in the correct way the depth camera

--- turtlebot3_gazebo/launch/turtlebot3_house.launch.py
+++ turtlebot3_gazebo/launch/turtlebot3_house.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='-2.0')
-    y_pose = LaunchConfiguration('y_pose', default='-0.5')
+    y_pose = LaunchConfiguration('y_pose', default='-1.5')
 
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
